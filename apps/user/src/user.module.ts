7c806b78ffdec9c71fa20b6743b526c0e9d76c5f
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2023-05-25 10:30:06 +0100

    fix: bug when using configModule and typeOrmModule

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -2,16 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import {User} from "./user.entity"
+import { User } from "./user.entity"
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database.module';
+import { joiSchema } from './joi.schema';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joiSchema()
+    }),
     DatabaseModule,
-    TypeOrmModule.forFeature([User])],
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule {
+  constructor() { }
+}
