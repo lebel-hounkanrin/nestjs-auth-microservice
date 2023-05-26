616bacd51676a911bd73a166bc94abcf6607a6cc
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2023-05-26 09:41:35 +0100

    feat: validate facebook user

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -36,4 +36,16 @@ export class AuthService {
       access_token: this.jwtService.sign(payload),
     };
   }
+
+  async findUserByFacebookIdOrEmail(id: string, email: string): Promise<any>{
+    return new Promise((resolve, reject) => {
+      this.client.send({role: "user", cmd: "get"}, {id, email}).subscribe(
+        user => resolve(user)
+      )
+    })
+  }
+
+  async createFacebookUser(id:string, email: string, displayName: string): Promise<any>{
+
+  }
 }
--- apps/auth/src/facebook.strategy.ts
+++ apps/auth/src/facebook.strategy.ts
@@ -3,19 +3,32 @@ import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { VerifyCallback } from "passport-jwt";
 import { Strategy } from "passport-local";
+import { AuthService } from "./auth.service";
 
 @Injectable()
-export class FacebookStrategy extends PassportStrategy(Strategy, "facebook"){
-    constructor(private configService: ConfigService){
+export class FacebookStrategy extends PassportStrategy(Strategy, "facebook") {
+    constructor(private configService: ConfigService, private authService: AuthService) {
         super({
             clientID: configService.get('FACEBOOK_APP_ID'),
             clientSecret: configService.get('FACEBOOK_APP_SECRET'),
             callbackURL: 'http://localhost:3000/auth/facebook/callback',
-            scope: ['email'],
+            scope: ['email', 'user_location'],
         })
     }
 
     async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+        try {
+            const { id, emails, displayName } = profile;
+            const email = emails?.[0]?.value;
+            const user = await this.authService.findUserByFacebookIdOrEmail(id, email);
+            if(user){
+                done(null, user)
+            } else{
+                const newUser = await this.authService.createFacebookUser(id, email, displayName);
+                done(null, newUser)
+            }
 
+        } catch (err) {
+        }
     }
 }
